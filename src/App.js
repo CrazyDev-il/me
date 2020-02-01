4a3151118bdf4c9f354e46bd96b5fbaf5a7e4d81
Chandra Sekhar Tiwari <chandrasekhartiwari@gmail.com>
2020-02-01 15:34:22 +0530

    testing deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Some Change to check
+          Some Change to check(new)
         </p>
         <a
           className="App-link"
